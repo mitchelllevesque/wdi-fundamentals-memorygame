aae2547a65d15abd3cc3275a2225fbf96b871006
Levesque <ML81034@AD2.PROD>
2018-05-02 09:25:33 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+var cardOne = "Queen of Diamonds";
+var cardTwo = "Queen of Hearts";
+var cardThree = "King of Diamonds";
+var cardFour = "King of Hearts";
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardTwo);
+console.log("User Flipped " + cardThree);
+console.log("User Flipped " + cardFour);
