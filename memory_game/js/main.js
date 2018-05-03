877b5b2aec1f44beaf5878f87392721df19afa2d
Levesque <ML81034@AD2.PROD>
2018-05-03 10:47:32 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,44 @@
-console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-heats.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-heats.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
-var checkForMatch = function () {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+var checkForMatch = function () 
+{
+if (cardsInPlay.length === 2)
+if (cardsInPlay[0] === cardsInPlay[1] ) 
+{
+    alert("You've found a match!");
     } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
     };
 };
 var flipCard = function (cardId) {
-    console.log("User flipped" + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped" + " " + cards[cardId].rank);
+    console.log(" " + cards[cardId].cardImage);
+    console.log(" " + cards[cardId].suit);
+    cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 };
 flipCard(0);
 flipCard(2);
-console.log(cardsInPlay);
 
-if (cardsInPlay[0] === cardsInPlay[1] ) {
-    alert("You've found a match!");
-    } else {
-        alert("Sorry, try again.");
-    };
     
