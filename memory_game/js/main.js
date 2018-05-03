cbcf33b765e88cb9ce7272ad3700451906be2bb1
Levesque <ML81034@AD2.PROD>
2018-05-03 08:41:50 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,25 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+var checkForMatch = function () {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+    } else {
+    console.log("Sorry, try again.");
+    };
+};
+var flipCard = function (cardId) {
+    console.log("User flipped" + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+checkForMatch();
+};
+flipCard(0);
+flipCard(2);
+console.log(cardsInPlay);
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped" + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped" + cardTwo);
-
-if (cardsInPlay[0] === cardsInPlay[2] ) {
-alert("You've found a match!");
-} else {
-    alert("Sorry, try again.");
-}
+if (cardsInPlay[0] === cardsInPlay[1] ) {
+    alert("You've found a match!");
+    } else {
+        alert("Sorry, try again.");
+    };
+    
